0eb7d0596177bf280df9bc91fe7bc60851636c11
KGNagarajan <nagarajankg1999@gmail.com>
2026-07-23 16:58:32 +0530

    Added home route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Maxima Spin Wheel Backend Running ✅");
+});
+
 // Random Coupon Generator
 function generateCoupon() {
   const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
